68460aa8bbea6069f54fb89380f49f9d0148b4f6
AshStorm17 <aashmun.gupta@gmail.com>
2026-02-16 16:10:18 +0530

    updated eval for using all datasets

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import random
-from dataset import load_adult, get_llm_probabilities, label_words
+from dataset import load_sst2, get_llm_probabilities, label_words
 from selection import *
 from evaluation import evaluate
 from plotting import plot_results
 
 # Load Adult
-train_data, test_data, formatter, label_fn = load_adult()
+train_data, test_data, formatter, label_fn = load_sst2()
 
 # Build candidates
 NUM_CANDIDATES = 150
 candidates = [formatter(train_data[i]) for i in range(NUM_CANDIDATES)]
+
+USE_DATASET = "sst2"
+if USE_DATASET == "adult":
+    group_key = "sex"
+else:
+    group_key = None
 
 K = 3
 
@@ -34,7 +40,8 @@ for name, func in strategies:
         formatter,
         get_llm_probabilities,
         label_words,
-        label_fn=label_fn
+        label_fn=label_fn,
+        group_key=group_key
     )
 
     print(f"{name}: accuracy={acc:.3f}, fairness={fairness:.3f}, gap={gap:.3f}")
